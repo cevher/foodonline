9b155a8d21a408d189aaeba7a0ad8ea36cd70675
cevher ozden <efeozden@gmail.com>
2025-09-16 15:16:53 +0300

    homepage template and database connection

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, welcome to the Food Online homepage!")
+    return render(request, "home.html")
